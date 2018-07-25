33b083dd737b4766786537a4ca032f74e5572d73
GVent <gavinven@yahoo.com>
2018-07-25 23:14:40 +0100

    JS snippet for supporting dnt as a way of turning of GA

--- getGAtoHonourDNT.js
+++ getGAtoHonourDNT.js
@@ -1,5 +1,8 @@
 /* functon that causes analytics.js or gat.js to honour DNT
 */
 if(navigator.doNotTrack == 1) {
-    // set value to true;
+    // set value to true and call before the GA snippet;
+    window['ga-disable-UA-XXXXX-Y'] = true;
+    // e.g .  window['ga-disable-UA-1000000-1'] = true;
+
 }
